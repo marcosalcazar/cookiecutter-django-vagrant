f5365a63783c18f96c0d13e123aee78951b0cd35
Marcos Gabriel Alcázar <marcos.alcazar@gmail.com>
2015-06-04 18:40:12 -0300

    Django 1.8 project structure

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/wsgi.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/wsgi.py
@@ -4,18 +4,18 @@ WSGI config for {{cookiecutter.repo_name}} project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
+
 import os
 from os.path import abspath, dirname
 from sys import path
+ 
+from django.core.wsgi import get_wsgi_application
 
 SITE_ROOT = dirname(dirname(abspath(__file__)))
 path.append(SITE_ROOT)
 
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.repo_name}}.settings")
 
-
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
