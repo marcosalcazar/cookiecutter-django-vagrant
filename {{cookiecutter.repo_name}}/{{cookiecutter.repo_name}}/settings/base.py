24f008207b0cd487f1955a14c297f6107a70062e
Marcos Gabriel Alcázar <marcos.alcazar@gmail.com>
2015-07-01 10:17:08 -0300

    Added STATIC_ROOT config

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
@@ -110,6 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = root('static')
 
 MEDIA_ROOT = root('assets', 'uploads')
 MEDIA_URL = '/media/'
