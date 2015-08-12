45914a8e5df9fa5a6ee6959c6ff7bd7a2bb5c19a
Marcos Gabriel Alcázar <marcos.alcazar@gmail.com>
2015-08-12 20:20:05 -0300

    Using 'root()' for sqlite db

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings/base.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = '{{cookiecutter.repo_name}}.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': root('db.sqlite3'),
     }
 }
 
